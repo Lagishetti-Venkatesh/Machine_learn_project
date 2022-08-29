d1a185298ce7e45271cc03031cb8b7c25a9a8b7d
Lagishetti-Venkatesh <venkatesh29.12.1997@gmail.com>
2022-08-29 18:46:56 +0530

    pipeline created

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return "Sample Flask app To test Git changes"
+    return "CI CD pipeline got created"
 
 if __name__ == "__main__":
     app.run(debug=True)
